89e4b13542a8b0cd14a410a44388808f4b2504a5
Yang Liu <jeremyrobturtle@gmail.com>
2020-07-16 16:45:30 -0700

    setup sentry for ci

--- config/environments/ci.rb
+++ config/environments/ci.rb
@@ -46,4 +46,10 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+
+  # Monitoring
+  Raven.configure do |config|
+    config.dsn = Rails.application.credentials&.sentry&.dig(:dsn)
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  end
 end
