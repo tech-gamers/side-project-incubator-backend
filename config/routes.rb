21ce291b7b7a2d22bece4bd74647685d123bbb9e
Yang Liu <jeremyrobturtle@gmail.com>
2020-07-16 21:13:00 -0700

    cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
+  # API
   namespace :api, defaults: { format: 'json' }, except: %i[new edit] do
+    delete 'logout', to: '/devise/sessions#destroy'
+
     resources :users do
       resources :auths, only: %i[index show destroy]
     end
   end
 
+  # Mount frontend
   root to: 'pages#home'
 
+  # Authentication
   devise_for :auths,
              controllers: {
-               omniauth_callbacks: 'callbacks/omniauth', sessions: 'sessions'
+               omniauth_callbacks: 'callbacks/omniauth'
              }
 end
