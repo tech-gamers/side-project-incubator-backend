7fd0c7a4395537f7f2faaa1ae3c362082a2484d0
Yang Liu <jeremyrobturtle@gmail.com>
2020-07-16 17:11:41 -0700

    [DEMO] mount a React component
    webpack not found

---
+++ app/javascript/components/HelloWorld.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import PropTypes from "prop-types"
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <p>
+        Greeting: {this.props.greeting}
+      </p>
+    );
+  }
+}
+
+HelloWorld.propTypes = {
+  greeting: PropTypes.string
+};
+export default HelloWorld
