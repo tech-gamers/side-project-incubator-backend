52a408782baee2f2ba0c286f1509fe549fbd78a1
Yang Liu <jeremyrobturtle@gmail.com>
2020-07-20 22:10:01 -0700

    set resources defaults

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  def api_resources(res, *args, &block)
+    defaults = {
+      format: :json,
+      only: %i[index show create update destroy]
+    }
+    # this will be changed in Rails 6.1
+    if args.length.positive? && args.last.is_a?(Hash)
+      args.last.merge!(defaults)
+    else
+      args << defaults
+    end
+    resources res, *args, &block
+  end
+
   devise_for :auths,
              controllers: {
                omniauth_callbacks: 'omniauth'
@@ -6,7 +20,7 @@ Rails.application.routes.draw do
 
   delete 'logout', to: 'devise/sessions#destroy'
 
-  resources :users do
-    resources :auths, only: %i[show index destroy]
+  api_resources :users do
+    api_resources :auths, except: %i[create]
   end
 end
