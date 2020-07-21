a4bf101733f128e8f833d6a318b7e8dcbeba4777
Yang Liu <jeremyrobturtle@gmail.com>
2020-07-21 01:11:28 -0700

    [HOTFIX] fix redirection URL for alpha testing

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -2,7 +2,7 @@ class OmniauthController < Devise::OmniauthCallbacksController
   def github
     @auth = Auth.from_github(request.env['omniauth.auth'])
     sign_in(@auth)
-    redirect_to @auth.user
+    redirect_to "https://alpha.tech-gamers.live/users/#{@auth.user_id}"
   end
 
   def failure
