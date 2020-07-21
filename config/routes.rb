52ef14226970ceb2f36353624454f12bcba9ee3b
Yang Liu <jeremyrobturtle@gmail.com>
2020-07-20 21:41:17 -0700

    fix path

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
                omniauth_callbacks: 'omniauth'
              }
 
-  delete 'logout', to: '/devise/sessions#destroy'
+  delete 'logout', to: 'devise/sessions#destroy'
 
   resources :users do
     resources :auths, only: %i[show index destroy]
